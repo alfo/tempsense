67de85cf813120735a603791b47cd2bf18390d6e
Alex Forey <me@alexforey.com>
2017-11-02 13:53:08 +0000

    Validation pls

--- Client/client.rb
+++ Client/client.rb
@@ -44,7 +44,11 @@ while true do
     # Remove any excess whitespace, and convert to float
     message = message.chomp.to_f
 
-    send_to_server message
+    # Sometimes the Arduino restarts mid-way through sending Serial data
+    # Validate data to make sure we're not sending something like 2122.52
+    # (Arduino prints 21, restarts, then prints 22.52)
+    send_to_server message if message.between?(-20, 60)
+
   end
 
 end
--- app/models/point.rb
+++ app/models/point.rb
@@ -1,2 +1,5 @@
 class Point < ApplicationRecord
+
+  validates :data, presence: true, inclusion: -20..60
+
 end
